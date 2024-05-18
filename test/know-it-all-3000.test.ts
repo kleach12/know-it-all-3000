f6051089f29bb2998dcb6d936d4274dcae01ccbe
Kale Leach <kaleleach@Jarmans-MacBook-Pro.local>
2024-05-18 15:29:43 -0700

    Adding AWS CDK

---
+++ test/know-it-all-3000.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KnowItAll3000 from '../lib/know-it-all-3000-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/know-it-all-3000-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KnowItAll3000.KnowItAll3000Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
